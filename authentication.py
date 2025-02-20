703e6cfce1dee5eddbb08c215ede3191dd61cb81
tmannix <84071602+tmannix@users.noreply.github.com>
2025-02-20 14:43:58 +0000

    yes please

--- authentication.py
+++ authentication.py
@@ -1,24 +1,27 @@
 import streamlit as st
+import requests
 
 def save_credentials(username, password):
-    st.secrets["username"] = username
-    st.secrets["password"] = password
+    st.session_state["username"] = username
+    st.session_state["password"] = password
 
 def get_auth_token():
-    import requests
-    auth = (st.secrets["username"], st.secrets["password"])
+    auth = (st.session_state.get("username"), st.session_state.get("password"))
     response = requests.get("https://api.anaplan.com/token", auth=auth)
     return response.json().get("token")
 
 st.title("Authentication Page")
 
-username = st.text_input("Username")
-password = st.text_input("Password", type="password")
+username = st.text_input("Username", value=st.session_state.get("username", ""))
+password = st.text_input("Password", type="password", value=st.session_state.get("password", ""))
 
 if st.button("Save Credentials"):
     save_credentials(username, password)
     st.success("Credentials saved!")
 
 if st.button("Get Auth Token"):
-    token = get_auth_token()
-    st.text_input("Auth Token", value=token, type="password")
+    if "username" in st.session_state and "password" in st.session_state:
+        token = get_auth_token()
+        st.text_input("Auth Token", value=token, type="password")
+    else:
+        st.error("Please save your credentials first.")
